1ab2c3af08611be412b9e7bbdb64c3a373c64f78
RudranshBose12 <gamingredking5@gmail.com>
2026-09-21 03:49:09 +0530

    Set Varanasi Connect GitHub repository configuration

--- site-config.js
+++ site-config.js
@@ -6,8 +6,8 @@ window.VC_CONFIG = {
   github: {
     // If your site is hosted at https://USERNAME.github.io/REPOSITORY/ these can be left as-is.
     // For a custom domain, put your GitHub username and repository name here.
-    owner: "YOUR_GITHUB_USERNAME",
-    repo: "YOUR_REPOSITORY_NAME",
+    owner: "RudranshBose12",
+    repo: "Varanasi-connect",
     branch: "main"
   },
   // Put your Google Form / Microsoft Form / other submission form here.
